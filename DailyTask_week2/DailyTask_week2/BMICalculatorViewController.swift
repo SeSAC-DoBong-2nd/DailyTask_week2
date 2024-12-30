431e6f0aedb1c31f94c12cf42606c57e5344dfc8
ParkSY0919 <qhr498@gmail.com>
2024-12-30 20:27:19 +0900

    [Add/#2] add BMICalculator Related files

---
+++ DailyTask_week2/DailyTask_week2/BMICalculatorViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BMICalculatorViewController.swift
+//  DailyTask_week2
+//
+//  Created by 박신영 on 12/30/24.
+//
+
+import UIKit
+
+class BMICalculatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
