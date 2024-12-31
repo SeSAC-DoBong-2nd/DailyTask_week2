a25a62737fc9e84e0ccb7d9c3f05a54ddbe6eb46
ParkSY0919 <qhr498@gmail.com>
2024-12-31 19:53:25 +0900

    [Add/#5] Tamagotchi files

---
+++ DailyTask_week2/DailyTask_week2/DailyTask241231/Tamagotchi/TamagotchiViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TamagotchiViewController.swift
+//  DailyTask_week2
+//
+//  Created by 박신영 on 12/31/24.
+//
+
+import UIKit
+
+class TamagotchiViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
