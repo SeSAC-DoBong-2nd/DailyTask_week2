fdf0b343185b518e3b04a92ebc9144fc206923ca
ParkSY0919 <qhr498@gmail.com>
2025-01-02 05:20:33 +0900

    [Feat/#4] AutoLayout Easy

---
+++ DailyTask_week2/DailyTask_week2/DailyTask241231/AutoLayout/AutoLayoutViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AutoLayoutViewController.swift
+//  DailyTask_week2
+//
+//  Created by 박신영 on 1/2/25.
+//
+
+import UIKit
+
+class AutoLayoutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
