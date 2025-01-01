9b11e122bdac8b0bd2287f2f56c7cde9e7805829
ParkSY0919 <qhr498@gmail.com>
2025-01-02 05:26:26 +0900

    [Add/#4] add AutoLayoutNaver Related files

---
+++ DailyTask_week2/DailyTask_week2/DailyTask241231/AutoLayout/AutoLayoutNaverViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AutoLayoutViewController.swift
+//  DailyTask_week2
+//
+//  Created by 박신영 on 1/2/25.
+//
+
+import UIKit
+
+class AutoLayoutNaverViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .black
+    }
+    
+
+
+}
