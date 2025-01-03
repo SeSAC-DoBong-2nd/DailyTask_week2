58747beda75e77283b1964f173eb76c85c75c022
ParkSY0919 <qhr498@gmail.com>
2025-01-03 23:35:43 +0900

    [Feat/#12] 세 번째 탭바 - UI 구현

--- DailyTask_week2/DailyTask_week2/DailyTask250103/ShoppingTap/ShoppingTableViewController.swift
+++ DailyTask_week2/DailyTask_week2/DailyTask250103/ShoppingTap/ShoppingTableViewController.swift
@@ -8,82 +8,103 @@
 import UIKit
 
 class ShoppingTableViewController: UITableViewController {
-
+    
+    var shoppingList = ["그립톡 구매하기", "사이다 구매", "아이패드 케이스 최저가 알아보기", "양말"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
-    // MARK: - Table view data source
-
+    
+    func setShoppingHeaderCell
+    (cell: ShoppingTableViewHeaderCell) {
+        cell.contentView.backgroundColor = .background0103
+        
+        cell.shoppingTextField.placeholder = "무엇을 구매하실 건가요?"
+        cell.shoppingTextField.borderStyle = .none
+        
+        cell.addButton.setTitle("추가", for: .normal)
+        cell.addButton.backgroundColor = .addButton0103
+        cell.addButton.layer.cornerRadius = 10
+        cell.addButton.setTitleColor(.black, for: .normal)
+    }
+    
+    func setShoppingMainCell(cell: ShoppingTableViewCell) {
+        cell.contentView.backgroundColor = .background0103
+        
+        cell.checkButton.setImage(UIImage(systemName: "checkmark.square"), for: .normal)
+        cell.checkButton.tintColor = .black
+        
+        cell.shoppingLabel.font = .systemFont(ofSize: 14, weight: .medium)
+        cell.shoppingLabel.textAlignment = .left
+        
+        cell.starButton.setImage(UIImage(systemName: "star"), for: .normal)
+        cell.starButton.tintColor = .black
+    }
+    
+    @objc
+    func addButtonTapped(_ sender: UIButton) {
+        print(#function, sender.tag)
+    }
+    
+    //section 개수 설정
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 2
     }
-
+    
+    //섹션 1과 섹션 2 사이 여백을 위해 footerHeight 설정
+    override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 15
+    }
+    
+    //cell 높이 설정
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 0 {
+            return 70
+        } else {
+            return 50
+        }
+    }
+    
+    //cell 개수
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        if section == 0 {
+            return 1
+        } else {
+            return shoppingList.count
+        }
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+        let headerCell = tableView.dequeueReusableCell(withIdentifier: "ShoppingTableViewHeaderCell") as! ShoppingTableViewHeaderCell
+        setShoppingHeaderCell(cell: headerCell)
+        headerCell.addButton.tag = indexPath[0]
+        headerCell.addButton.addTarget(self, action: #selector(addButtonTapped), for: .touchUpInside)
+        
+        let mainCell = tableView.dequeueReusableCell(withIdentifier: "ShoppingTableViewCell") as! ShoppingTableViewCell
+        setShoppingMainCell(cell: mainCell)
+        
+        switch indexPath.section {
+        case 0:
+            return headerCell
+        case 1:
+            mainCell.shoppingLabel.text = shoppingList[indexPath.row]
+            return mainCell
+        default:
+            return UITableViewCell()
+        }
+        
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(#function, indexPath.row)
+        
+        tableView.reloadData()
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    //HeaderCell textField 액션 함수
+    @IBAction
+    func shoppingTextFieldEditingChanged(_ sender: UITextField) {
+        print(#function)
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
