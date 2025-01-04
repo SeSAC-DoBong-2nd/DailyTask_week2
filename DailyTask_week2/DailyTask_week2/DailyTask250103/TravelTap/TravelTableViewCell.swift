08d88a863a76d81e061cb65e38ab1193118c208d
ParkSY0919 <qhr498@gmail.com>
2025-01-04 20:27:20 +0900

    [Feat/#12] 두 번째 탭바 - TravelTableViewCell outlet 배치

--- DailyTask_week2/DailyTask_week2/DailyTask250103/TravelTap/TravelTableViewCell.swift
+++ DailyTask_week2/DailyTask_week2/DailyTask250103/TravelTap/TravelTableViewCell.swift
@@ -9,15 +9,10 @@ import UIKit
 
 class TravelTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var subtitleLabel: UILabel!
+    @IBOutlet var etcLabel: UILabel!
+    @IBOutlet var travelImageView: UIImageView!
+    @IBOutlet var heartButton: UIButton!
+    
 }
