f4d7a6caea4eebd2584d539368c1de49521e12bd
ParkSY0919 <qhr498@gmail.com>
2025-01-04 19:57:16 +0900

    [Add/#12] 두 번째 탭바 - add Travel files

---
+++ DailyTask_week2/DailyTask_week2/DailyTask250103/TravelTap/TravelModel.swift
@@ -0,0 +1,8 @@
+//
+//  TravelModel.swift
+//  DailyTask_week2
+//
+//  Created by 박신영 on 1/4/25.
+//
+
+import Foundation
---
+++ DailyTask_week2/DailyTask_week2/DailyTask250103/TravelTap/TravelTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TravelTableViewCell.swift
+//  DailyTask_week2
+//
+//  Created by 박신영 on 1/4/25.
+//
+
+import UIKit
+
+class TravelTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
