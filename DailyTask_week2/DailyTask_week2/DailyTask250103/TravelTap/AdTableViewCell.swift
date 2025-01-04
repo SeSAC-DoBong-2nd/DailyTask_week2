8149ff1a3084733933f633b0fa2544179c42977d
ParkSY0919 <qhr498@gmail.com>
2025-01-04 21:23:29 +0900

    [Feat/#12] 두 번째 탭바 - AdTableViewCell outlet 배치

--- DailyTask_week2/DailyTask_week2/DailyTask250103/TravelTap/AdTableViewCell.swift
+++ DailyTask_week2/DailyTask_week2/DailyTask250103/TravelTap/AdTableViewCell.swift
@@ -9,15 +9,8 @@ import UIKit
 
 class AdTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    @IBOutlet var adBackgroundView: UIView!
+    @IBOutlet var adTitleLabel: UILabel!
+    @IBOutlet var adButton: UIButton!
+    
 }
